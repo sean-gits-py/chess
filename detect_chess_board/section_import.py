a69f7aea91b6ad9dda11488b42bdb6e5c71f3211
Sean <sean_github@pm.me>
2024-08-12 14:23:09 -0700

    Update section_import.py

--- detect_chess_board/section_import.py
+++ detect_chess_board/section_import.py
@@ -1,23 +1,28 @@
 # section_import.py
 # imports book contents as "sections"
 import csv
+import pprint
 
-#function creation
-def load_sections(section_file:str="cm_sections.csv") -> dict:
+# Function to load puzzle information from csv file
+def load_sections(section_file: str = "cm_sections.csv") -> dict:
+    """Load section information from csv file into dict."""
     sections = {}
     with open(section_file, 'r') as csvfile:
-        fieldnames = ["section_id","title","start_page","end_page","first_puzzle_id"]
+        fieldnames = ["section_id", "title", "start_page", "end_page", "first_puzzle_id"]
         csvreader = csv.DictReader(csvfile, fieldnames=fieldnames)
-        next(csvreader, None) # skip header
+        next(csvreader, None)  # skip header
         for row in csvreader:
-            chapters[int(row['section_id'])] = {
+            sections[int(row['section_id'])] = {
                 "title": row['title'],
                 "start_page": int(row['start_page']),
                 "end_page": int(row['end_page']),
                 "first_puzzle_id": int(row['first_puzzle_id']),
             }
-
     return sections
 
+# PrettyPrinter
+pp = pprint.PrettyPrinter(indent=4)
+
+# Load and print sections
 sections = load_sections()
 pp.pprint(sections)
